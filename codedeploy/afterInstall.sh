6a9070f7554ec969e5402f883f1b28f915477d63
jinshuang <niu.jins@northeastern.edu>
2022-04-02 03:54:25 -0700

    gh

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -3,7 +3,7 @@ sudo systemctl stop tomcat
 sudo rm -rf /tmp/demo1-0.0.1-SNAPSHOT
 sudo chown tomcat:tomcat /tmp/demo1-0.0.1-SNAPSHOT.jar
 
-# cleanup log files
+# cleanup log file
 sudo rm -rf /tmp/logs/catalina*
 sudo rm -rf /tmp/logs/*.log
 sudo rm -rf /tmp/logs/*.txt
