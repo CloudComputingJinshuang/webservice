85f6e4617f56f60286cc6157c09c71074efc6bbf
jinshuang <niu.jins@northeastern.edu>
2022-04-02 14:34:22 -0700

    crontab-4

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # sudo systemctl start tomcat
+cd /tmp
+java -jar demo1-0.0.1-SNAPSHOT.jar --spring.config.location=file:///tmp/jdbc.properties
 crontab -l | { cat; echo "@reboot (cd /tmp && java -jar demo1-0.0.1-SNAPSHOT.jar --spring.config.location=file:///tmp/jdbc.properties)"; } | crontab -
 # cd /tmp
 # java -jar demo1-0.0.1-SNAPSHOT.jar --spring.config.location=file:///tmp/jdbc.properties
