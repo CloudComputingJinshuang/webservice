4acce7a12fa32ae9d39908429b7dd7b682c9e1f7
jinshuang <niu.jins@northeastern.edu>
2022-04-02 16:41:49 -0700

    crontab-7

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -8,5 +8,15 @@
 # # Do stuff
 # kill $MyPID                     # kill PID (can happen later in script as well)
 
-pkill -f 'java -jar'
-sudo rm -rf /tmp/demo1-0.0.1-SNAPSHOT.jar
+# pkill -f 'java -jar'
+# sudo rm -rf /tmp/demo1-0.0.1-SNAPSHOT.jar
+
+sleep 120
+cd /tmp
+pwd
+ls -al
+if sudo kill "$(cat ./pid.file)"; then
+  echo Killed Application
+else
+  echo no webapp found
+fi
