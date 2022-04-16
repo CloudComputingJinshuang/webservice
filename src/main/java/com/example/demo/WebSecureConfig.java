d6bae283ec129b433e2c1cd331b2bb391589c4aa
jinshuang <niu.jins@northeastern.edu>
2022-04-15 23:13:47 -0700

    a09

--- src/main/java/com/example/demo/WebSecureConfig.java
+++ src/main/java/com/example/demo/WebSecureConfig.java
@@ -44,7 +44,7 @@ public class WebSecureConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/*", "/v1/user*", "/v1/user/","/v1/user","/v2/user*", "/v2/user/","/v2/user").permitAll()
+                .antMatchers("/*", "/v1/user*", "/v1/user/","/v1/user","/v2/user*", "/v2/user/","/v2/user","/v1/*","/v1/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
